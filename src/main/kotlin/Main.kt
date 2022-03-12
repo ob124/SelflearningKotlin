bfa604eab4c4012b3e26ebf5ffc27d15b3600127
ob124 <ob124.oliver@gmail.com>
2022-03-12 20:31:02 +1300

    completed part 4 of kotlin fundamentals course.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,42 @@
 import com.rsk.Person
 import java.io.FileReader
 import java.io.IOException
+import java.math.BigInteger
 import java.util.*
 
 fun main(args: Array<String>) {
     displayExample("Hello, World.")
+    log(logLevel = 2, message = "test")
+    println("Hello, World.".AddOne())
+
+    println(fib(10000, BigInteger("1"), BigInteger("0")))
 
 }
+
+
+// tail recursive helps avoid stack overflows
+tailrec fun fib(n: Int, a: BigInteger, b: BigInteger): BigInteger {
+    return if (n == 0) b else fib(n-1, a+b, a)
+}
+
+
 fun displayExample(message:String) : Boolean {
     println(message)
     return true
+}
+
+
+// extension function example
+fun String.AddOne() : String {
+    var one = "1"
+    return this+one
+}
+
+
+// default parameter + override
+@JvmOverloads
+fun log(message: String, logLevel: Int = 1) {
+    println(message + logLevel)
 }
 
 
@@ -49,7 +76,7 @@ fun basics() {
     q?.Answer = "41"  // question mark says if q isn't null then do it. Does a null check for you
     q?.display()
 
-    println("The answer to the question ${q?.Question} is ${q?.Answer}")
+    println("The answer to the question ${q?.question} is ${q?.Answer}")
     q?.printResult()
 
 
@@ -100,10 +127,10 @@ fun printName(name: String){
 }
 
 
-class Question {
+class Question() {
     var Answer:String? = "" //Question mark means string can be null
     val CorrectAnswer = "42"
-    val Question: String = "What is the answer to life?"
+    var question: String = "What is the answer to life?"
 
     fun display() {
         println("You said $Answer")  // dollar is saying that its a variable
@@ -114,5 +141,12 @@ class Question {
             CorrectAnswer -> println("You were correct")
             else -> println("Try again?")
         }
+    }
+
+    // creates a plus function
+    infix fun Question.plus(other: Question) : Question {
+        var q = Question()
+        q.question = this.question + other.question
+        return q
     }
 }
